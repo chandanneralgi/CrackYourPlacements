b3cf4c138e40347af067a3e662ac970ff5730520
chandanneralgi <106388395+chandanneralgi@users.noreply.github.com>
2024-07-15 22:39:55 +0530

    fourth question

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -12,6 +12,7 @@ class Solution {
                 }
             }
         }
+        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(row[i] || col[j]){
