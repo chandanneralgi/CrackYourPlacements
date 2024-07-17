d9d7b53f90c4d6d2cba0db95fd02d4b2f3dfb6e5
chandanneralgi <106388395+chandanneralgi@users.noreply.github.com>
2024-07-17 20:26:38 +0530

    22

---
+++ Maximum Points You Can Obtain from Cards.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int totalSum = 0;
+        for (int point : cardPoints) {
+            totalSum += point;
+        }
+
+        int windowSize = n - k;
+        int currentWindowSum = 0;
+        for (int i = 0; i < windowSize; i++) {
+            currentWindowSum += cardPoints[i];
+        }
+
+        int minWindowSum = currentWindowSum;
+        for (int i = windowSize; i < n; i++) {
+            currentWindowSum += cardPoints[i] - cardPoints[i - windowSize];
+            minWindowSum = Math.min(minWindowSum, currentWindowSum);
+        }
+        
+        return totalSum - minWindowSum;
+    }
+}
